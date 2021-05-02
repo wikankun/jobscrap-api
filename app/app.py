4ee368d24affa0fbfad3fddcd5ecf2ae3c36af63
Wikan <wikan25@gmail.com>
2021-05-02 11:53:50 +0700

    fix object insertion error

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Security,Depends, HTTPException
 from fastapi.security.api_key import APIKeyQuery, APIKey
+from pydantic import BaseModel
+from typing import List
 from app.db.mongo import *
 from dotenv import load_dotenv
 import json
@@ -26,6 +28,18 @@ async def get_api_key(api_key_query: str = Security(api_key_query)):
         )
 
 
+class Jobs(BaseModel):
+    job_id: str
+    job_title: str
+    company: str
+    job_post_date: str
+    job_requirement_career_level: str
+    company_size: str
+    company_industry: str
+    job_description: str
+    job_employment_type: str
+    job_function: str
+
 app = FastAPI(redoc_url=None)
 db = Mongo(MONGO_URL, DATABASE, COLLECTION)
 
@@ -39,6 +53,6 @@ async def show_job(job_id, api_key: APIKey = Depends(get_api_key)):
     return db.show(job_id)
 
 @app.post("/jobs/")
-async def upsert_job(docs, api_key: APIKey = Depends(get_api_key)):
-    return db.upsert(json.loads(docs))
+async def upsert_job(jobs: List[Jobs], api_key: APIKey = Depends(get_api_key)):
+    return db.upsert([job.__dict__ for job in jobs])
 
